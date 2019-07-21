ea930bb56d579ab34b577fd020cbf9b7ba0a62ad
Anitra <anitra.oursky@gmail.com>
2019-07-21 17:41:17 -0400

    update

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        name:"",
+        photo:"",
+        scores: []
+    },
+    {
+        name:"",
+        photo:"",
+        scores: []
+    },
+    {
+        name:"",
+        photo:"",
+        scores: []
+    },
+];
+
+module.exports = friends;
